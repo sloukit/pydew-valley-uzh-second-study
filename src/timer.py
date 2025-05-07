e6e9473ae1f2c6e400ac9a87f814457737ec3df9
fortwoone <denauxarthur@gmail.com>
2025-05-07 16:13:51 +0200

    First version of the bubble animation when taking a bath

--- src/timer.py
+++ src/timer.py
@@ -5,6 +5,8 @@ class Timer:
     def __init__(self, duration, repeat=False, autostart=False, func=None):
         self.duration = duration
         self.start_time = 0
+        self.now = 0
+        self.current_duration = 0
         self.active = False
         self.finished = False
         self.repeat = repeat
@@ -20,11 +22,15 @@ class Timer:
         self.active = True
         self.finished = False
         self.start_time = pygame.time.get_ticks()
+        self.now = pygame.time.get_ticks()
+        self.current_duration = 0
 
     def deactivate(self):
         self.active = False
         self.finished = True
         self.start_time = 0
+        self.now = 0
+        self.current_duration = 0
         if self.repeat:
             self.activate()
 
@@ -32,11 +38,14 @@ class Timer:
         """returns a value between 0 and 1 that shows the timers progress
         1 means duration finshed"""
         curr = pygame.time.get_ticks()
-        return (curr - self.start_time) / self.duration if self.active else 0
+        return self.current_duration / self.duration if self.active else 0
 
     def update(self):
         if self.active:
-            if pygame.time.get_ticks() - self.start_time >= self.duration:
+            self.start_time = self.now
+            self.now = pygame.time.get_ticks()
+            self.current_duration += self.now - self.start_time
+            if self.current_duration >= self.duration:
                 if self.func and self.start_time != 0:
                     self.func()
                 self.deactivate()
